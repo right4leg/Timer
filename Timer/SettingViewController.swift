c24e367ccff03d53d2e30d113306bb84faf1d441
right4leg <right4leg0727@icloud.com>
2020-06-25 20:46:46 +0900

    Setting側のコード書き込み

--- Timer/SettingViewController.swift
+++ Timer/SettingViewController.swift
@@ -8,17 +8,67 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
-
+class SettingViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        //1列なのでreturn1
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //settingArrayの数だけ行を表示する
+        return settingArray.count
+    }
+    
+    //UIPickerViewの表示する内容を設定
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(settingArray[row])
+    }
+    
+    //Picker選択時に実行
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //UserDefaultの設定
+        let settings = UserDefaults.standard
+        settings.setValue(settingArray[row], forKey: settingKey)
+        //UserDefaultのデータを永続化
+        settings.synchronize()
+    }
+    
+    //UIPickerViewに表示するデータをArrayで作成
+    let settingArray : [Int] = [10,20,30,40,50,60]
+    //設定値を覚えるキーを設定
+    let settingKey = "timer_Value"
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //Timesettingのデリゲートとデータソースの通知先を指定
+        Timesetting.dataSource = self
+        Timesetting.delegate = self
+        
+        //UserDefaultの取得
+        let settings = UserDefaults.standard
+        let timerValue = settings.integer(forKey: settingKey)
+        
+        //Pickerの選択を合わせる
+        for row in 0..<settingArray.count {
+            if settingArray[row] == timerValue {
+                Timesetting.selectRow(row, inComponent: 0, animated: true)
+            }
+        }
+        
+        
     }
     
     @IBOutlet weak var Timesetting: UIPickerView!
     
     @IBAction func Enter(_ sender: Any) {
+        //前画面に戻る
+        _ = navigationController?.popViewController(animated: true)
     }
     
     
